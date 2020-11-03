71119cad5c82a02a8647e683370d722d8cbe37e9
Will Estes <westes575@gmail.com>
2020-11-03 16:23:38 -0500

    update configs

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
-# Require TF version to be same as or greater than 0.12.13
 terraform {
-  required_version = ">=0.12.13"
-  #backend "s3" {
-  #  bucket         = "your_globally_unique_bucket_name"
-  #  key            = "terraform.tfstate"
-  #  region         = "us-east-1"
-  #  dynamodb_table = "aws-locks"
-  #  encrypt        = true
-  #}
+  required_version = ">=0.13.0"
+  backend "s3" {
+    bucket         = "hubert-s3-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "aws-locks"
+    encrypt        = true
+  }
 }
 
 # Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
@@ -16,9 +15,8 @@ provider "aws" {
   version = "~> 2.36.0"
 }
 
-# Call the seed_module to build our ADO seed info
 module "bootstrap" {
-  source                      = "./modules/bootstrap"
-  name_of_s3_bucket           = "your_globally_unique_bucket_name"
-  dynamo_db_table_name        = "aws-locks"
+  source               = "./modules/bootstrap"
+  name_of_s3_bucket    = "hubert-s3-bucket"
+  dynamo_db_table_name = "aws-locks"
 }
